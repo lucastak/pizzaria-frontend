3235a6d5f29917c2afc97829f2bf63a99990e59f
lucastak <contato.lucas.santos@hotmail.com>
2024-01-30 18:24:34 -0300

    feat: adiciona estilizacao da pagina de dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,6 +3,8 @@ import type { NextPage } from "next";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import Head from "next/head";
 import { Header } from "../../components/Header";
+import styles from "./styles.module.scss"
+import { FiRefreshCcw } from "react-icons/fi";
 
 
 const Dashboard: NextPage = () => {
@@ -12,11 +14,31 @@ const Dashboard: NextPage = () => {
         <Head>
             <title>Painel Pizzaria</title>
         </Head> 
-        <Header />
+        
         <div>
-          <h1>
-            Painel
-          </h1>
+          <Header />
+        
+          <main className={styles.container}>
+            
+            <div className={styles.containerHeader}>
+              
+              <h1>Últimos Pedidos</h1>
+              <button>
+                <FiRefreshCcw color="#3fffa3" />
+              </button>
+            </div>
+          
+            <article className={styles.listOrders}>
+
+                <section className={styles.orderItem}>
+                  <button>
+                    <div className={styles.tag}></div>
+                    <span>Mesa 30</span>
+                  </button>
+                </section>
+              
+            </article>
+          </main>
         </div>
       </>
   );
